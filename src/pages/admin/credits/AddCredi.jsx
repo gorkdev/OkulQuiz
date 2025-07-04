c61b09246200399072b2b78282f187801b4338cc
Ercan <ercanpolat@gmail.com>
2025-07-04 17:09:20 +0300

    deneme

--- src/pages/admin/credits/AddCredi.jsx
+++ src/pages/admin/credits/AddCredi.jsx
@@ -27,7 +27,7 @@ const AddCredi = () => {
         setSchools(data);
       } catch (err) {
         // Hata yönetimi
-        alert("Okullar yüklenirken hata oluştu");
+        alert("Okullar yüklenirken hata olmuştur");
       } finally {
         setLoading(false);
       }
