bb47d0db86bddf0a791bd40dcfc200558d1ed896
Gorkem <uvyk@icloud.com>
2025-07-16 17:31:27 +0300

    Sorular icin kategori eklendi, listelendi, guncelleme ve silme gibi aksiyonlarda eklendi.

---
+++ src/pages/admin/questions/AddNewQuestion.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminHeader from "../../../components/AdminHeader/AdminHeader";
+
+const AddNewQuestion = () => {
+  return (
+    <>
+      <AdminHeader title="Yeni Soru Ekle" />
+    </>
+  );
+};
+
+export default AddNewQuestion;
